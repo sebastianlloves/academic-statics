0e995ede984161a902aefe8e956ad8f6bb35403a
sebastianlloves <sebastianlloves@gmail.com>
2024-06-14 15:59:33 -0300

    .

--- src/app/table/data-table.tsx
+++ src/app/table/data-table.tsx
@@ -1,8 +1,8 @@
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel, PaginationState } from '@tanstack/react-table'
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel, getFilteredRowModel, ColumnFiltersState } from '@tanstack/react-table'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { useState } from 'react'
 import { Button } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
+import FiltersHead from './filters/filtersHead'
+import { useState } from 'react'
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -10,29 +10,55 @@ interface DataTableProps<TData, TValue> {
 }
 
 export function DataTable<TData, TValue> ({ columns, data }: DataTableProps<TData, TValue>) {
-  const [pagination, setPagination] = useState<PaginationState>({ pageSize: 100, pageIndex: 0 })
-
+  const [filter, setFilter] = useState<ColumnFiltersState>([])
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel({ initialSync: true }),
-    onPaginationChange: setPagination,
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnFiltersChange: setFilter,
+    initialState: {
+      pagination: { pageIndex: 0, pageSize: 100 }
+    },
     state: {
-      pagination
+      columnFilters: filter
     }
   })
-  // console.log(table.getHeaderGroups())
 
   return (
     <>
+      <FiltersHead table={table} />
       <div className='border rounded-md'>
         <Table>
-          <TableHeader className='bg-background/95'>
-            <TableRow key={table.getHeaderGroups()[1].id}>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      className='py-2' style={{
+                        width: header.column.getSize(),
+                        maxWidth: header.column.getSize()
+                      }} key={header.id}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          {/* <TableHeader className='bg-background/95'>
+            <TableRow key={table.getHeaderGroups()[0].id}>
               {table.getHeaderGroups()[0].headers.map((header, index) => {
-                console.log(header)
-                if (header.isPlaceholder) {
+                console.log(table.getAllLeafColumns())
+                if (table.getHeaderGroups()[1].headers[index].isPlaceholder) {
                   const header = table.getHeaderGroups()[1].headers[index]
                   return (
                     <TableHead key={header.id} colSpan={header.colSpan} className='text-foreground text-center'>
@@ -55,7 +81,7 @@ export function DataTable<TData, TValue> ({ columns, data }: DataTableProps<TDat
               })}
 
             </TableRow>
-          </TableHeader>
+          </TableHeader> */}
           <TableBody className=''>
             {table.getRowModel().rows?.length
               ? (table.getRowModel().rows.map((row) => (
--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className='relative h-96 w-full overflow-auto'>
+  <div className='relative w-full overflow-auto'>
     <table
       ref={ref}
       className={cn('w-full caption-bottom text-sm', className)}
@@ -20,7 +20,7 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn('[&_tr]:border-b sticky top-0', className)} {...props} />
+  <thead ref={ref} className={cn('[&_tr]:border-b', className)} {...props} />
 ))
 TableHeader.displayName = 'TableHeader'
 
