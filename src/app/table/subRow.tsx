d2b80982d6e36a321b4287703937b391816c2a4d
sebastianlloves <sebastianlloves@gmail.com>
2024-08-01 15:12:16 -0300

    .

--- src/app/table/subRow.tsx
+++ src/app/table/subRow.tsx
@@ -15,12 +15,7 @@ function SubRow ({ triggerContent, subjects, open } : SubRowProps) {
           <div className='flex items-center h-10'>
             {triggerContent === 0
               ? (
-                <Badge
-                  variant='outline'
-                  className='text-sm text-success bg-success/5 font-medium border-0'
-                >
-                  {triggerContent}
-                </Badge>
+                <Badge variant='outline' className='text-xs px-3 rounded-md font-normal text-success border-0 bg-success/5'>No adeuda</Badge>
                 )
               : (
                 <Badge
@@ -32,15 +27,15 @@ function SubRow ({ triggerContent, subjects, open } : SubRowProps) {
                 )}
           </div>
         </CollapsibleTrigger>
-        <CollapsibleContent className='mt-2 mb-2 ml-4 -z-50'>
-          <div className='flex flex-col items-start space-y-1.5'>
-            {subjects.map(subject => (
-              subject === 'No adeuda'
-                ? <Badge variant='outline' className='text-xs px-3 rounded-md font-normal text-success border-0 bg-success/5' key={subject}>{subject}</Badge>
-                : <Badge variant='outline' className='text-xs px-3 rounded-md font-normal' key={subject}>{subject}</Badge>
-            ))}
-          </div>
-        </CollapsibleContent>
+        {triggerContent > 0 && (
+          <CollapsibleContent className='mt-2 mb-2 ml-4 -z-50'>
+            <div className='flex flex-col items-start space-y-1.5'>
+              {subjects.map(subject => (
+                <Badge variant='outline' className='text-xs px-3 rounded-md font-normal' key={subject}>{subject}</Badge>
+              ))}
+            </div>
+          </CollapsibleContent>
+        )}
       </Collapsible>
     </>
   )
